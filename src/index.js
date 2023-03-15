97b2c415f851505f427e65507d791905b8ba96f3
ellinarel <113920230+ellinarel@users.noreply.github.com>
2023-03-15 23:16:58 +0200

    update

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ import { fetchImages } from "./fetchimages";
 const refs = {
     formEl : document.querySelector('.search-form')
 }
+
+refs.formEl.addEventListener(`submit`, onSubmit)
+function onSubmit(e){
+    e.preventDefault();
+    const searchValue = refs.formEl.elements.searchQuery.value.trim();
+    fetchImages(searchValue)
+    .then((response) => {
+      console.log(response.data);
+    })
+}
